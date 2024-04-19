7cd654cba56b2a0cd1226f3c027e055d734ce0ab
AqilBayramli <aqil.bayramli25@gmail.com>
2024-04-19 18:11:15 +0400

    v2

--- js/main.js
+++ js/main.js
@@ -70,55 +70,55 @@ jQuery(document).ready(function ($) {
   }
 
   // Smoth scroll on page hash links
-  $('a[href*="#"]:not([href="#"])').on('click', function () {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+  // $('a[href*="#"]:not([href="#"])').on('click', function () {
+  //   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 
-      var target = $(this.hash);
-      if (target.length) {
-        var top_space = 0;
+  //     var target = $(this.hash);
+  //     if (target.length) {
+  //       var top_space = 0;
 
-        if ($('#header').length) {
-          top_space = $('#header').outerHeight();
+  //       if ($('#header').length) {
+  //         top_space = $('#header').outerHeight();
 
-          if (!$('#header').hasClass('header-fixed')) {
-            top_space = top_space - 20;
-          }
-        }
+  //         if (!$('#header').hasClass('header-fixed')) {
+  //           top_space = top_space - 20;
+  //         }
+  //       }
 
-        $('html, body').animate({
-          scrollTop: target.offset().top - top_space
-        }, 1500, 'easeInOutExpo');
+  //       $('html, body').animate({
+  //         scrollTop: target.offset().top - top_space
+  //       }, 1500, 'easeInOutExpo');
 
-        if ($(this).parents('.nav-menu').length) {
-          $('.nav-menu .menu-active').removeClass('menu-active');
-          $(this).closest('li').addClass('menu-active');
-        }
+  //       if ($(this).parents('.nav-menu').length) {
+  //         $('.nav-menu .menu-active').removeClass('menu-active');
+  //         $(this).closest('li').addClass('menu-active');
+  //       }
 
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-          $('#mobile-body-overly').fadeOut();
-        }
-        return false;
-      }
-    }
-  });
+  //       if ($('body').hasClass('mobile-nav-active')) {
+  //         $('body').removeClass('mobile-nav-active');
+  //         $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+  //         $('#mobile-body-overly').fadeOut();
+  //       }
+  //       return false;
+  //     }
+  //   }
+  // });
 
   // Porfolio filter
-  $("#portfolio-flters li").click(function () {
-    $("#portfolio-flters li").removeClass('filter-active');
-    $(this).addClass('filter-active');
+  // $("#portfolio-flters li").click(function () {
+  //   $("#portfolio-flters li").removeClass('filter-active');
+  //   $(this).addClass('filter-active');
 
-    var selectedFilter = $(this).data("filter");
-    $("#portfolio-wrapper").fadeTo(100, 0);
+  //   var selectedFilter = $(this).data("filter");
+  //   $("#portfolio-wrapper").fadeTo(100, 0);
 
-    $(".portfolio-item").fadeOut().css('transform', 'scale(0)');
+  //   $(".portfolio-item").fadeOut().css('transform', 'scale(0)');
 
-    setTimeout(function () {
-      $(selectedFilter).fadeIn(100).css('transform', 'scale(1)');
-      $("#portfolio-wrapper").fadeTo(300, 1);
-    }, 300);
-  });
+  //   setTimeout(function () {
+  //     $(selectedFilter).fadeIn(100).css('transform', 'scale(1)');
+  //     $("#portfolio-wrapper").fadeTo(300, 1);
+  //   }, 300);
+  // });
 
   // jQuery counterUp
   $('[data-toggle="counter-up"]').counterUp({
